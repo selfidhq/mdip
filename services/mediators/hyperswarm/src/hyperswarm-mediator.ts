0885b4db1720e0b437f7299bf631da59b7003cd8
Bushstar <bushsolo@gmail.com>
2026-02-13 14:47:25 +0000

    suppress lint warnings

--- services/mediators/hyperswarm/src/hyperswarm-mediator.ts
+++ services/mediators/hyperswarm/src/hyperswarm-mediator.ts
@@ -886,6 +886,7 @@ function shouldAdvanceToOlderWindow(session: PeerSyncSession): boolean {
 
 async function planRuntimeWindows(): Promise<ReconciliationWindow[]> {
     if (!negentropyAdapter) {
+        // eslint-disable-next-line sonarjs/no-duplicate-string
         throw new Error('negentropy adapter unavailable');
     }
 
