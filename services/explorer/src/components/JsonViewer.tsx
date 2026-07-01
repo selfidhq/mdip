987db6670e2a1eb3d2101263e984275d54b4c19e
Edge-Man <shawn@selfid.com>
2026-07-01 12:54:59 -0400

    chore: update redis db connectors 7

--- services/explorer/src/components/JsonViewer.tsx
+++ services/explorer/src/components/JsonViewer.tsx
@@ -25,22 +25,8 @@ import { useSnackbar } from "../contexts/SnackbarProvider.js";
 import { handleCopyDID } from "../shared/utilities.js";
 import { useSearchParams } from "react-router-dom";
 
-<<<<<<< HEAD
-const searchServerURL = import.meta.env.VITE_SEARCH_SERVER || "/search-api";
-const VERSION = '/api/v1';
-
-function JsonViewer(
-    {
-        gatekeeper,
-        setError,
-    }: {
-        gatekeeper: GatekeeperInterface;
-        setError: (error: any) => void;
-    }) {
-=======
 function JsonViewer() {
     const { setError } = useSnackbar();
->>>>>>> origin/main
     const [aliasDocs, setAliasDocs] = useState<MdipDocument | undefined>(undefined);
     const [aliasDocsVersion, setAliasDocsVersion] = useState<number>(1);
     const [aliasDocsVersionMax, setAliasDocsVersionMax] = useState<number>(1);
