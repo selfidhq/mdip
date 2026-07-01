ec1d3d0e3b72932db9e92d05b08f51738bc57d07
Edge-Man <shawn@selfid.com>
2026-07-01 12:47:45 -0400

    chore: update redis db connectors 5

--- services/explorer/src/App.tsx
+++ services/explorer/src/App.tsx
@@ -8,18 +8,10 @@ import {
     Typography,
 } from "@mui/material";
 import Header from "./components/Header.js";
-import { GatekeeperEvent } from "@mdip/gatekeeper/types";
-import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
-
-const gatekeeper = new GatekeeperClient();
-
-interface SnackbarState {
-    open: boolean;
-    message: string;
-    severity: AlertColor;
-}
-
-const gatekeeperUrl = import.meta.env.VITE_GATEKEEPER_URL || '/api';
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import type { GatekeeperEvent } from "@mdip/gatekeeper/types";
+import { useSnackbar } from "./contexts/SnackbarProvider.js";
+import { useExplorerContext } from "./contexts/ExplorerProvider.js";
 
 function App() {
     const { setError } = useSnackbar();
